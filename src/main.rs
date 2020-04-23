5b05ae8ac2c4e7d557c56402df49c9a8c9a822ca
Ujjwal Sharma <ryzokuken@disroot.org>
2020-04-23 18:36:15 +0530

    fetch all secret keys

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate gpgme;
+
 fn main() {
-    println!("Hello, world!");
+    let mut ctx = gpgme::Context::from_protocol(gpgme::Protocol::OpenPgp).unwrap();
+    for key in ctx.secret_keys().unwrap() {
+        println!("{:?}", key);
+    }
 }
