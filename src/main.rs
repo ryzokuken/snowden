79096d2cd03b0220781b6ae53823f9342caf7d79
Ujjwal Sharma <ryzokuken@disroot.org>
2020-04-25 13:44:31 +0530

    set max line width to 80 characters

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,36 @@ extern crate toml;
 extern crate xdg;
 
 fn get_key() -> gpgme::Key {
-    let xdg_dirs =
-        xdg::BaseDirectories::with_prefix("snowden").expect("cannot find config directory");
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("snowden")
+        .expect("cannot find config directory");
     let config_path = xdg_dirs
         .find_config_file("config.toml")
         .expect("config.toml not found");
-    let config = std::fs::read_to_string(config_path).expect("error reading config.toml");
+    let config = std::fs::read_to_string(config_path)
+        .expect("error reading config.toml");
     let value = config
         .parse::<toml::Value>()
         .expect("config.toml is invalid");
     let fpr = value["key"].as_str().expect("key fingerprint invalid");
-    let mut ctx =
-        gpgme::Context::from_protocol(gpgme::Protocol::OpenPgp).expect("failed to connect to gpg");
+    let mut ctx = gpgme::Context::from_protocol(gpgme::Protocol::OpenPgp)
+        .expect("failed to connect to gpg");
     ctx.get_secret_key(fpr).expect("invalid key specified")
 }
 
 fn get_repo() -> git2::Repository {
     let path = std::env::current_dir().expect("current directory is invalid");
-    git2::Repository::open(path).expect("current directory is not a valid git repository")
+    git2::Repository::open(path)
+        .expect("current directory is not a valid git repository")
 }
 
 fn commit(key: gpgme::Key, repo: git2::Repository, msg: &str) {
     let head = repo.head().expect("error fetching head");
     let target = head.target().unwrap();
     let object = repo.find_object(target, None).unwrap();
-    let prev_commit = object.as_commit().expect("head doesn't point to a commit");
-    let signature = git2::Signature::now(key.id().unwrap(), "anon@ymo.us").unwrap();
+    let prev_commit =
+        object.as_commit().expect("head doesn't point to a commit");
+    let signature =
+        git2::Signature::now(key.id().unwrap(), "anon@ymo.us").unwrap();
     repo.commit(
         Some("HEAD"),
         &signature,
